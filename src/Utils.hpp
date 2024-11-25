b340f81accab9406d66217a0c2e2443b2db18290
Hir0Yat0 <93908562+Hir0Yat0@users.noreply.github.com>
2024-11-26 00:23:04 +0700

    feat(Utils): add arrays printing

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -3,12 +3,13 @@
 
 #include<iostream>
 #include<vector>
+#include<array>
 
 #include "Vec3D.hpp"
 
 namespace Utils
 {
-    
+
 #define DBGEXPR(EXPR) {std::cerr << #EXPR << ": " << EXPR << "\n";}
 #define DBGEXPROUT(EXPR) {std::cout << #EXPR << ": " << EXPR << "\n";}
 
@@ -27,6 +28,49 @@ inline void printVectorErr(const std::vector<T> & vec){
         std::cerr << data << ", ";
     }
     std::cerr << "]\n";
+}
+
+// template<class T, size_t N>
+// inline void printArray(const std::array<T,N> & arr){
+//     std::cout << "[";
+//     for (const auto &data : arr){
+//         std::cout << data << ", ";
+//     }
+//     std::cout << "]\n";
+// }
+// template<class T, size_t N>
+// inline void printArrayErr(const std::array<T,N> & arr){
+//     std::cerr << "[";
+//     for (const auto &data : arr){
+//         std::cerr << data << ", ";
+//     }
+//     std::cerr << "]\n";
+// }
+
+#define printArray(arr) {\
+    std::cout << "["; \
+    for (const auto &data : arr){ \
+        std::cout << data << ", "; \
+    } \
+    std::cout << "]\n"; \
+}
+
+#define printArrayErr(arr){ \
+    std::cerr << "["; \
+    for (const auto &data : arr){ \
+        std::cerr << data << ", "; \
+    } \
+    std::cerr << "]\n"; \
+}
+
+#define DBGEXPRARRAY(EXPRARRAY) { \
+    std::cerr << #EXPRARRAY << ": "; \
+    printArrayErr(EXPRARRAY); \
+}
+
+#define DBGEXPRARRAYOUT(EXPRARRAY) { \
+    std::cerr << #EXPRARRAY << ": "; \
+    printArray(EXPRARRAY); \
 }
 
 }; // namespace Utils
