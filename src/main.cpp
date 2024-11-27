8c4363bb62dbcbb48d6861b8fe762cf6614fc84b
Hir0Yat0 <93908562+Hir0Yat0@users.noreply.github.com>
2024-11-28 01:58:53 +0700

    test: added test code runner in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,20 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+#include "Utils.hpp"
+
+#include "Tester.hpp"
+#include "TestSphereRender.hpp"
+
 int main(){
     
     std::cout << "Hello Worlds!" << "\n";
     
-    return 0;
+    auto tester = TestSphereRender();
+
+    int exitcode = tester.runTest();
+
+    DBGEXPR(exitcode);
+
+    return exitcode;
 }
