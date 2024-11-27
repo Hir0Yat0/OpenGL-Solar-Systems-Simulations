db779c10866d413f791eb2eb5f1de71dc7165e1b
Hir0Yat0 <93908562+Hir0Yat0@users.noreply.github.com>
2024-11-28 01:58:19 +0700

    refactor(GLDrawWindow): commented out previous ShapeSurface draw

--- src/GLDrawWindow.hpp
+++ src/GLDrawWindow.hpp
@@ -17,17 +17,27 @@ class GLDrawWindow
 private:
     /* data */
     GLFWwindow * window;
-    bool polygonFillMode;
     const int SCR_WIDTH;
     const int SCR_HEIGHT;
+    bool polygonFillMode;
     static void framebuffer_size_callback(GLFWwindow * window, int width, int height);
 public:
     int initSuccess;
     GLDrawWindow(/* args */);
     ~GLDrawWindow();
+
+    /* this makes -Werror=effc++ compiles for GLFWwindow * window; */
+    /* https://stackoverflow.com/a/53892135 */
+
+    GLDrawWindow(const GLDrawWindow&) = delete;              // copy ctor
+    GLDrawWindow(GLDrawWindow&&) = delete;                   // move ctor
+    GLDrawWindow& operator=(const GLDrawWindow&) = delete;   // copy assignment
+    GLDrawWindow& operator=(GLDrawWindow&&) = delete;        // move assignment
+
+
     int drawWindow(Shader &shaderProgram, Shape & shape,const std::optional<Texture> & texture);
     int drawWindow(Shader &shaderProgram, std::vector<Shape> & shapes,const std::optional<Texture> & texture);
-    int drawWindow(Shader &shaderProgram, std::vector<ShapeSurface> & shapeSurfaces)    ;
+    // int drawWindow(Shader &shaderProgram, std::vector<ShapeSurface> & shapeSurfaces)    ;
     void togglePolygonFillMode();
     void processInput();
     int initGLFWWindow();
