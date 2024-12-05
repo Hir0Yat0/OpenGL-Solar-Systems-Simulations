d882dc525d70e2cd8244ff2e6f62e6de6c38a2cd
Hir0Yat0 <93908562+Hir0Yat0@users.noreply.github.com>
2024-12-05 22:43:07 +0700

    feat(Camera): added [[nodiscard]] to GetViewMatrix

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -57,7 +57,7 @@ class Camera {
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
 
     // Returns the view matrix
-    // [[no_discard]]
+    [[nodiscard]]   
     glm::mat4 GetViewMatrix() ;
 
     
