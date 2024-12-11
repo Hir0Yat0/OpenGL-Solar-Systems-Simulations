7fefb97d6dd884b913b14ea4eb2c5e11357bb169
Hir0Yat0 <93908562+Hir0Yat0@users.noreply.github.com>
2024-12-11 19:48:40 +0700

    feat: added planetary scales

---
+++ src/Scales.hpp
@@ -0,0 +1,36 @@
+#ifndef SCALES_HPP
+#define SCALES_HPP
+
+namespace Scales {
+    
+    constexpr float distanceFromSunScale = 1.0f / 1.0e+6;
+    constexpr float diameterScale = 1.0f / 1.0e+5;
+
+    constexpr float starSunDiameterKM = 1391400.0f;
+    constexpr float planetSunDiameterKM = 1391400.0f;
+    constexpr float planetMercuryDiameterKM = 4879.0f;
+    constexpr float planetVenusDiameterKM = 12104.0f;
+    constexpr float planetEartDiameterKM = 12756.0f;
+    constexpr float planetMarsDiameterKM = 6792.0f;
+    constexpr float planetJupiterDiameterKM = 142984.0f;
+    constexpr float planetSaturnDiameterKM = 120536.0f;
+    constexpr float planetUranusDiameterKM = 51118.0f;
+    constexpr float planetNeptuneDiameterKM = 49528.0f;
+    constexpr float planetPlutoDiameterKM = 2376.6f;
+
+    constexpr float startSunDistanceFromSunKM = 1.0f;
+    constexpr float planetSunDistanceFromSunKM = 1.0f;
+    constexpr float planetMercuryDistanceFromSunKM = 5.79e+7f;
+    constexpr float planetVenusDistanceFromSunKM = 1.082e+8f;
+    constexpr float planetEarthDistanceFromSunKM = 1.4960e+8f;
+    constexpr float planetMarsDistanceFromSunKM = 2.2790e+8f;
+    constexpr float planetJupiterDistanceFromSunKM = 7.7860e+8f;
+    constexpr float planetSaturnDistanceFromSunKM = 1.43350e+9f;
+    constexpr float planetUranusDistanceFromSunKM = 2.87250e+9f;
+    constexpr float planetNepturneDistanceFromSunKM = 4.49510e+9f;  
+    constexpr float planetPlutoDistanceFromSunKM = 5.9e+9f;
+
+}; // namespace Scales
+
+
+#endif // SCALES_HPP
